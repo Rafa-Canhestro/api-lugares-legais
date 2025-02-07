9964612ba9a38b6e7704516a86f28034517b3ca8
Rafa-Canhestro <133936986+Rafa-Canhestro@users.noreply.github.com>
2025-02-07 00:40:28 -0300

    Add files via upload

---
+++ api-lugares-legais/src/main/java/com/lugares/legais/api_lugares_legais/ApiLugaresLegaisApplication.java
@@ -0,0 +1,13 @@
+package com.lugares.legais.api_lugares_legais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLugaresLegaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLugaresLegaisApplication.class, args);
+	}
+
+}
---
+++ api-lugares-legais/src/test/java/com/lugares/legais/api_lugares_legais/ApiLugaresLegaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lugares.legais.api_lugares_legais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLugaresLegaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
